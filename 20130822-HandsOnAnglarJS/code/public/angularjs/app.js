b8ec2a1007fad793e5ce6b8fc00e509007c33f56
Robin Böhm <robinboehm@googlemail.com>
2013-08-22 10:25:41 +0200

    Copy HandsOnAngularJS for @WWRuhr specific slides

---
+++ 20130822-HandsOnAnglarJS/code/public/angularjs/app.js
@@ -0,0 +1,35 @@
+var app = angular.module('todomvc', ['ngResource']);
+
+app.factory('ToDoService', function ($resource) {
+    return $resource('/todos/:id', { id:'@id' }, { update:{method:'PUT' }})
+});
+
+app.controller('TodoCtrl', function ($scope, ToDoService) {
+    var todos = $scope.todos = ToDoService.query();
+
+    $scope.createTodo = function (newTodo) {
+        if (!newTodo.length) { return };
+
+        var todo = { title: newTodo.trim(), completed: false };
+        ToDoService.save(todo)
+            .$then(function (response) {
+                todos.push(response.data);
+            });
+        $scope.newTodo = '';
+    };
+
+    $scope.editTodo = function (todo) {
+        $scope.editedTodo = todo;
+    };
+
+    $scope.updateTodo = function (todo) {
+        $scope.editedTodo = null;
+        ToDoService.save(todo);
+    }
+
+    $scope.removeTodo = function (todo) {
+        todos.splice(todos.indexOf(todo), 1);
+        ToDoService.delete(todo);
+    };
+
+});
