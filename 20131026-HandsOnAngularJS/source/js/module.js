575fe749ba24bb8005c10256c206cc9cc2c63b7f
Robin Böhm <robinboehm@googlemail.com>
2013-12-11 14:22:55 +0100

    chore(*): remove duplicate content and organize repo with tags

--- 20131026-HandsOnAngularJS/source/js/module.js
+++
@@ -1,1 +0,0 @@
-angular.module("rboehmAngularSlides", ['angular-leap','goinstant']);
--- 20131130-CGM-Hamburg/source/js/syncScope.js
+++
@@ -1,5 +0,0 @@
-angular.module("rboehmAngularSlides")
-// Sync the whole new Scope
-.controller('syncScope', function ($scope, GoAngular) {
-    new GoAngular($scope, 'scope' + $scope.$id).initialize();
-});
